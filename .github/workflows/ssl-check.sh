af0730c9e4534a9c274f6e800a437f4573591b35
zubeirahamed <126669677+zubeirahamed@users.noreply.github.com>
2023-08-22 18:04:52 +0530

    Create ssl-check.sh

---
+++ .github/workflows/ssl-check.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+SLACK_WEBHOOK_URL=$SLACK_WEBHOOK_URL
+
+DOMAINS_FILE=".github/scripts/domains.txt"
+DOMAINS=($(cat "${DOMAINS_FILE}"))
+
+for domain in "${DOMAINS[@]}"; do
+  expiry_date=$(echo | openssl s_client -connect "${domain}:443" 2>/dev/null | openssl x509 -noout -dates | grep "notAfter" | cut -d= -f2)
+  expiry_epoch=$(date -d "${expiry_date}" +%s)
+  current_epoch=$(date +%s)
+  days_remaining=$(( (expiry_epoch - current_epoch) / 86400 ))
+
+  if [[ $days_remaining -lt 30 ]]; then
+    message="⚠ SSL Expiry Alert\n   * Domain : ${domain}\n   * Warning : The SSL certificate for ${domain} will expire in ${days_remaining} days."
+    payload="payload={\"text\":\"${message}\"}"
+    curl -s -X POST --data-urlencode "$payload" "$SLACK_WEBHOOK_URL"
+  fi
+done
